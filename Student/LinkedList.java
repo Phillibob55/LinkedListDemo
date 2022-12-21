0c0cc25fc069e33b642427e4efe81adeb58526f2
Tolly-Zhang <tollyzhang@gmail.com>
2022-12-21 01:02:16 -0800

    Added/Optimized Insert to LinkedList

--- Student/LinkedList.java
+++ Student/LinkedList.java
@@ -3,20 +3,74 @@ public class LinkedList {
     public LinkedList(){
         head = null;
     }
-
-    public void InsertAtHead(Student data){
-        /*
-        Create new node
-        Point its next to head
-        point head to this node
-        */
-        Node nd = new Node(data);
-        if(head != null){
-            nd.next = head;
+    public void insert(int index, Student data){
+        //Combines head, index, and tail insert
+        if(index == 0){
+            Node nd = new Node(data);
+            if(head != null){
+                nd.next = head;
+            }
+            head = nd; 
         }
-        head = nd;
+        else if(index == listLength()){
+            if(head == null){
+                Node nd = new Node(data);
+                head = nd;
+            }
+            else{
+                Node nd = new Node(data);
+                Node tail = findLast();
+                tail.next = nd;
+            }
+        }
+        else{
+            int i = 0;
+            Node trav = head;
+            while(i < index - 1){
+                trav = trav.next;
+                i++;
+            }
+            Node nd = new Node(data);
+            nd.next = trav.next;
+            trav.next = nd;
+        }
     }
-
+    // public void insertAtHead(Student data){
+    //     /*
+    //     Create new node
+    //     Point its next to head
+    //     point head to this node
+    //     */
+    //     Node nd = new Node(data);
+    //     if(head != null){
+    //         nd.next = head;
+    //     }
+    //     head = nd;
+    // }
+    // public void insertBeforeNode(int index, Student data){
+    //     int i = 0;
+    //     Node trav = head;
+    //     while(i < index - 1){
+    //         trav = trav.next;
+    //         i++;
+    //     }
+    //     Node nd = new Node(data);
+    //     nd.next = trav.next;
+    //     trav.next = nd;
+        
+    // }
+    // public void insertAtTail(Student data){
+    //     // When Linked List is empty
+    //     if(head == null){
+    //         Node nd = new Node(data);
+    //         head = nd;
+    //     }
+    //     else{
+    //         Node nd = new Node(data);
+    //         Node tail = findLast();
+    //         tail.next = nd;
+    //     }
+    // }
     public Node findLast(){
         if(head == null){
             return null;
@@ -26,19 +80,6 @@ public class LinkedList {
             trav = trav.next;
         }
         return trav;
-    }
-
-    public void InsertAtTail(Student data){
-        // When Linked List is empty
-        if(head == null){
-            Node nd = new Node(data);
-            head = nd;
-        }
-        else{
-            Node nd = new Node(data);
-            Node tail = findLast();
-            tail.next = nd;
-        }
     }
     public Node findPrevious(Node nd){
         Node previous = head;
@@ -76,6 +117,21 @@ public class LinkedList {
             trav = trav.next;
         }
         System.out.println();
+    }
+    public int listLength(){
+        if(head == null){
+            return 0;
+        }
+        else if(head.next == null){
+            return 1;
+        }
+        int i = 1;
+        Node trav = head;
+        while(trav.next != null){
+            trav = trav.next;
+            i++;
+        }
+        return i;
     }
 
 }
